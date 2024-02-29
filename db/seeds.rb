e2c707bf3c7a3e8e62015dde927953466217acff
Cassandra Dodd <cassy.dodd95@gmail.com>
2024-02-29 19:38:36 +0100

    FEAT: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+INDICATIONS = %w[hair diabetes flu].freeze
+
+10.times do
+  Doctor.create(name: Faker::Name.unique.first_name, email: Faker::Internet.unique.email,
+                speciality: INDICATIONS.sample, password: 'password1234', password_confirmation: 'password1234')
+end
--- spec/factories/doctor.rb
+++ spec/factories/doctor.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :doctor do
     name { 'Test' }
-    email { 'foo@bar.com' } 
-    speciality {'hair' }
+    email { 'foo@bar.com' }
+    speciality { 'hair' }
     password { 'password1234' }
   end
 end
